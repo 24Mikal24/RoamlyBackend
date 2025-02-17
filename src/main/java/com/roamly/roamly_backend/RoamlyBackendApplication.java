090a78ad09bc191e6eaf1ee06fb73e81a849fb87
Michael Archer <mikearcherdev@proton.me>
2025-02-17 06:01:18 -0600

    init commit

---
+++ src/main/java/com/roamly/roamly_backend/RoamlyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.roamly.roamly_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoamlyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoamlyBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/roamly/roamly_backend/RoamlyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roamly.roamly_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoamlyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
